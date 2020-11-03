9a83f8699969e8dae7a5f9b9c770ee9f7d2610c1
Josiah Travis <josiahtravis1@gmail.com>
2020-11-03 13:11:44 -0800

    acronym program

--- Program.cs
+++ Program.cs
@@ -1,5 +1,22 @@
+using System;
+using System.Collections.Generic;
+
 class Program {
     static void Main() {
-        System.Console.WriteLine("Hello World");
+
+        System.Console.WriteLine("Enter words:");
+
+        string input = System.Console.ReadLine();
+
+        string[] words = input.Split(" ");
+
+        string result = "";
+
+        foreach (var x in words) {
+            result+=x[0].ToString().ToUpper();
+        }
+
+        System.Console.WriteLine(result);
+
     }
 }
